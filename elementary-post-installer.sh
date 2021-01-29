e300eb08fe43f8246943745b2820432a692a5173
Alain Kouassi <aokouassi@gmail.com>
2021-01-29 15:32:55 -0500

    Added flatpak sources

--- elementary-post-installer.sh
+++ elementary-post-installer.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo apt install evolution slack texmaker firefox snapd steam flatpak teams gnome-maps -y
 sudo snap install --classic code subl spotify discord onlyoffice-desktopeditors intellij-idea-ultimate -y  
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
