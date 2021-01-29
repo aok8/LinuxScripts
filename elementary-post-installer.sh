e5cfdda483ea34e031480ed56429a7f48b0e6a48
Alain Kouassi <aokouassi@gmail.com>
2021-01-29 12:47:51 -0500

    added more programs

--- elementary-post-installer.sh
+++ elementary-post-installer.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo apt install 
+sudo apt install evolution slack texmaker firefox snapd steam flatpak -y
+sudo snap install --classic code subl spotify discord onlyoffice-desktopeditors intellij-idea-ultimate -y  
