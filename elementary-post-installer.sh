f514fa57bb3cd1a32a8fb55ea9a8b42f95aa8f02
Alain Kouassi <aokouassi@gmail.com>
2021-01-29 12:50:17 -0500

    added teams

--- elementary-post-installer.sh
+++ elementary-post-installer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt install evolution slack texmaker firefox snapd steam flatpak -y
+sudo apt install evolution slack texmaker firefox snapd steam flatpak teams -y
 sudo snap install --classic code subl spotify discord onlyoffice-desktopeditors intellij-idea-ultimate -y  
